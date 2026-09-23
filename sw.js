d3189ec0d47c86462b6c5964a1561d0e6667e4c5
soldato31 <syscomentertainment@gmail.com>
2026-09-23 15:42:37 +0200

    v3: autoapprendimento, percorso esami integrativi, malattia, supplenze, scrutini, Classroom, Importa smart

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Diario Serale - service worker (rete prima, cache come riserva)
-const C='diario-serale-v2';
+const C='diario-serale-v3';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET'||new URL(r.url).origin!==location.origin)return;
